4eab559f31d38b5579bd44523906de15ddb013b1
Colan Connan <colanconnan@Colans-MacBook-Pro.local>
2016-08-08 13:55:08 -0400

    Added multi line string and print it out.

--- main.php
+++ main.php
@@ -1,14 +1,29 @@
 <?php
-    require('./constants.php');
-    $array = [
-        "HELLO_WORLD" => HELLO_WORLD,
-        "TWO" => TWO,
-        "THREE" => THREE,
-        "MY_NAME" => MY_NAME,
-        "FIVE" => FIVE
-    ];
+require('./constants.php');
+$array = [
+"HELLO_WORLD" => HELLO_WORLD,
+"TWO" => TWO,
+"THREE" => THREE,
+"MY_NAME" => MY_NAME,
+"FIVE" => FIVE
+];
 
-    $result = $array["TWO"] * $array["THREE"];
+$result = $array["TWO"] * $array["THREE"];
 
-    echo "THE RESULTS OF " . $array["TWO"] ." * " . $array["THREE"] . " is: " . $result . "\n";
+$WORLD = "WORLD";
+$TEST = "TEST";
+$ONE  = 1;
+$TWO = 2;
+$THREE = 3;
+
+
+$multiLine = <<<HERE
+hello $WORLD!
+
+this a $TEST string...
+
+the sum of $ONE plus $TWO is $THREE \n
+HERE;
+
+echo $multiLine;
 ?>
