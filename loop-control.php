9b64643514b8e80b8447fd1a5e4097e7216cacce
Colan Connan <colanconnan@Colans-MacBook-Pro.local>
2016-08-08 14:37:35 -0400

    Foreach refacorted to  print out based on matching  conditions

--- loop-control.php
+++ loop-control.php
@@ -3,8 +3,19 @@
     $number = array_filter(range(0, 100), function($item) { return ($item % 3) == 0; });
 
     foreach($number as $num) {
-        echo ($num == 3 ? "Three \n" : "");
-        echo ($num == 9 ? "Nine Nine Nine \n" : "");
-        echo ($num == 15 ? "Fifteen Fifteen Fifteen Fifteen Fifteen \n" : "");
+        if($num % 7 == 0) {
+            echo "Sevens are lucky, this number has " . ($num / 7);
+            echo "\n";
+        }
+        else if ($num % 10 == 0){
+            echo $num . " is a round number";
+            echo "\n";
+        } 
+        if($num == $number[0]){
+            echo "First number \n";
+        }
+        if($num == end($number)){
+            echo "Last number \n";
+        }
     }
 ?>
