2c7d4c311eb261af0b9cb8cc22f81e7a4e63b4e5
Colan Connan <colanconnan@Colans-MacBook-Pro.local>
2016-08-08 13:36:01 -0400

    define the array of constants

---
+++ main.php
@@ -0,0 +1,13 @@
+<?php
+    require('./constants.php');
+    $array = [
+        "HELLO_WORLD" => HELLO_WORLD,
+        "TWO" => TWO,
+        "THREE" => THREE,
+        "MY_NAME" => MY_NAME,
+        "FIVE" => FIVE
+    ];
+
+
+
+?>
