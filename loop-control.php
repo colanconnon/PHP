2b12a0837d5336ad83579009ddc0ebda4bb302a7
Colan Connan <colanconnan@Colans-MacBook-Pro.local>
2016-08-08 14:11:24 -0400

    Add the foreach loop and echo the results base on condition.

--- loop-control.php
+++ loop-control.php
@@ -2,4 +2,9 @@
 
     $number = array_filter(range(0, 100), function($item) { return ($item % 3) == 0; });
 
+    foreach($number as $num) {
+        echo ($num == 3 ? "Three \n" : "");
+        echo ($num == 9 ? "Nine Nine Nine \n" : "");
+        echo ($num == 15 ? "Fifteen Fifteen Fifteen Fifteen Fifteen \n" : "");
+    }
 ?>
